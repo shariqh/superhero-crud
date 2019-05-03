c0a13721b38a09e451e67401cd173f2cc14d68ea
Shariq Hirani <shariqhirani2010@gmail.com>
2019-05-03 10:07:02 -0500

    scaffolding

---
+++ src/main/java/com/shariq/superheromicroservices/superherocrud/SuperheroCrudApplication.java
@@ -0,0 +1,13 @@
+package com.shariq.superheromicroservices.superherocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shariq/superheromicroservices/superherocrud/SuperheroCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shariq.superheromicroservices.superherocrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperheroCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
